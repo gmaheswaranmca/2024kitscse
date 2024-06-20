fe4049a793f52f8ac35004fe1ffa926ae061b49e
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-06-20 16:16:28 +0530

    numbers after added clearnig the box and get focus to the box

--- js/script-numbers.js
+++ js/script-numbers.js
@@ -4,8 +4,11 @@ function doAdd(){
     number_list.push(number);
     let items = '';
     for(let n of number_list) {
-        items = items + `<LI>${n}</LI>`
+        items = items + `<LI class="p-2 btn btn-primary m-2" 
+                style="list-style-type:none;">${n}</LI>`
     }
-    let numbers_html = `<UL>${items}</UL>`
+    let numbers_html = `<UL class="d-flex flex-wrap">${items}</UL>`
     document.getElementById("numbers-list").innerHTML = numbers_html;
+    document.getElementById('number').value = "";
+    document.getElementById('number').focus();
 }
